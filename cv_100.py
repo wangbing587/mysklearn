99a8790591c0dac93af594ab65e4af4bfafa1cf0
wangbing587 <39249841+wangbing587@users.noreply.github.com>
2021-06-11 23:31:27 +0800

    Update cv_100.py

--- cv_100.py
+++ cv_100.py
@@ -10,12 +10,14 @@ def train(model, x, y, epochs=100, scale=True, n_splits=5):
         x = StandardScaler().fit_transform(x)
     trainpred = np.zeros((x.shape[0], epochs))
     trainprob = np.zeros((x.shape[0], epochs))
+    
     for i in tqdm(range(epochs)):
         skf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=i)
-        for train_index, val_index in enumerate(skf.split(x, y)):
+        for train_index, val_index in skf.split(x, y):
             x_train, x_val = x[train_index], x[val_index]
             y_train, y_val = y[train_index], y[val_index]
             model.fit(x_train, y_train)
-            trainpred[val_index, i] = model.pred(x_val)
-            trainprob[val_index, i] = model.prob(x_val)
+            print(model.score(x_val, y_val))
+            trainpred[val_index, i] = model.predict(x_val)
+            trainprob[val_index, i] = model.predict_proba(x_val)[:, 1]
     return trainpred, trainprob
