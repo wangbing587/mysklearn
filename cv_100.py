787aebec94efc6979cbe596ca6ca9f4767b6fb4a
wangbing587 <39249841+wangbing587@users.noreply.github.com>
2021-06-11 22:51:31 +0800

    Create cv_100.py

---
+++ cv_100.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+
+
+def train(model, x, y, epochs=100, scale=True, n_splits=5):
+    if scale:
+        x = StandardScaler().fit_transform(x)
+    trainpred = np.zeros((x.shape[0], epochs))
+    trainprob = np.zeros((x.shape[0], epochs))
+    for i in tqdm(range(epochs)):
+        skf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=i)
+        for train_index, val_index in enumerate(skf.split(x, y)):
+            x_train, x_val = x[train_index], x[val_index]
+            y_train, y_val = y[train_index], y[val_index]
+            model.fit(x_train, y_train)
+            trainpred[val_index, i] = model.pred(x_val)
+            trainprob[val_index, i] = model.prob(x_val)
+    return trainpred, trainprob
